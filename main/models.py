09a5258514af2b91ae3c0deb8a8d029a1175b63c
koohanmo <rngksah@naver.com>
2016-11-25 00:34:54 +0900

    Project, Contacts Model 생성, 마이그레이트

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class project(models.Model):
+    title = models.CharField(max_length=40)
+    type = models.CharField(max_length=40)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    image = models.ImageField()
+
+
+class contact(models.Model):
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+    phone = models.CharField(max_length=13)
+    msg = models.TextField(blank=False)
+
